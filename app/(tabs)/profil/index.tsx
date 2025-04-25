737c32a73101e8dcd7138628bc8f2b975ec66d1e
adit-wr <ganding0002nd@gmail.com>
2025-04-25 12:48:05 +0700

    integration profile data

--- app/(tabs)/profil/index.tsx
+++ app/(tabs)/profil/index.tsx
@@ -6,22 +6,29 @@ import {
   TouchableOpacity,
   ScrollView,
   StatusBar,
+  Alert,
 } from "react-native";
 import Background from "../../components/background";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { images } from "@/constants/images";
 import Settings from "@/components/settings";
 import { ImageProvider, useImage } from "@/components/picker/imagepicker";
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+import { Ionicons } from "@expo/vector-icons";
 
-const DataDummy = {
-  id: 1,
-  nama: "Dr Izzu Adit Intan Nita",
-  username: "Zuditanit",
-  email: "zuditanit@gmail.com",
-  no_tlp: "08123712953234",
-  spesialis: "Jantung",
-};
+interface User {
+  nama_dokter: string;
+  username_dokter: string;
+  email_dokter: string;
+  spesialis_dokter: string;
+  str_dokter: string;
+  notlp_dokter: string;
+  rating_dokter: string;
+  foto_profil_dokter: string | null;
+}
+
 
 export default function ProfileScreen() {
   return (
@@ -34,6 +41,77 @@ export default function ProfileScreen() {
 function App() {
   const imageContext = useImage();
   const profileImage = imageContext?.profileImage || null;
+  const [userData, setUserData] = useState<User | null>(null);
+  const [passwordLama, setPasswordLama] = useState("");
+  const [passwordBaru, setPasswordBaru] = useState("");
+  const [konfirmasiPassword, setKonfirmasiPassword] = useState("");
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = async () => {
+    try {
+      const userId = await SecureStore.getItemAsync("userId");
+      const cleanedUserId = userId?.replace(/"/g, "");
+      if (cleanedUserId) {
+        const response = await axios.get(
+          `https://mjk-backend-five.vercel.app/api/dokter/getbyid/${cleanedUserId}`
+        );
+        setUserData(response.data);
+      }
+    } catch (error) {
+      console.error("Gagal mengambil data profil:", error);
+    }
+  };
+
+  if (!userData) {
+    return (
+      <Background>
+        <View className="flex-1 justify-center items-center">
+          <Text className="text-skyDark text-xl font-bold">
+            Memuat profil...
+          </Text>
+        </View>
+      </Background>
+    );
+  }
+
+  const handleGantiPassword = async () => {
+    try {
+      const token = await SecureStore.getItemAsync("userToken");
+
+      if (!token) {
+        return Alert.alert(
+          "Gagal",
+          "Token tidak ditemukan, silakan login ulang"
+        );
+      }
+
+      const res = await axios.patch(
+        "https://mjk-backend-five.vercel.app/api/dokter/ubah-password",
+        {
+          password_lama: passwordLama,
+          password_baru: passwordBaru,
+          konfirmasi_password_baru: konfirmasiPassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      Alert.alert("Berhasil", res.data.message);
+      setPasswordLama("");
+      setPasswordBaru("");
+      setKonfirmasiPassword("");
+    } catch (error: any) {
+      const msg =
+        error.response?.data?.message ||
+        "Terjadi kesalahan saat mengubah password";
+      Alert.alert("Gagal", msg);
+    }
+  };
 
   return (
     <Background>
@@ -43,12 +121,9 @@ function App() {
           {/* Header */}
           <View className="relative pt-12 bg-skyLight rounded-b-[50px] py-28">
             <View className="absolute inset-0 flex items-center justify-between flex-row px-12">
-              <Text className="text-skyDark text-2xl font-bold">
-                Selamat datang, {"\n"}
-                {DataDummy.nama}
-              </Text>
+              <Text className="text-skyDark text-2xl font-bold">Profile</Text>
               <Image
-                className="h-10 w-12 "
+                className="h-10 w-12"
                 source={images.logo}
                 resizeMode="contain"
               />
@@ -57,21 +132,15 @@ function App() {
 
           {/* Foto Profil */}
           <View className="absolute top-28 left-1/2 -translate-x-1/2">
-            {profileImage ? (
-              <View className="border-4 border-skyDark rounded-full p-1">
-                <Image
-                  source={{ uri: profileImage }}
-                  className="w-32 h-32 rounded-full"
-                />
-              </View>
+            {userData.foto_profil_dokter ? (
+              <Image
+                source={{ uri: userData.foto_profil_dokter }}
+                className="w-32 h-32 rounded-full border-4 border-skyDark"
+              />
             ) : (
-              <Text className="border-4 border-skyDark rounded-full ">
-                <MaterialCommunityIcons
-                  name="account-circle"
-                  size={120}
-                  color="grey"
-                />
-              </Text>
+              <View className="w-32 h-32 rounded-full border-4 border-skyDark items-center justify-center bg-gray-200">
+                <Ionicons name="person" size={64} color="#0C4A6E" />
+              </View>
             )}
           </View>
 
@@ -86,26 +155,26 @@ function App() {
             }}
           >
             <Text className="font-bold text-lg text-skyDark">Nama</Text>
-            <Text className="text-gray-700">{DataDummy.nama}</Text>
+            <Text className="text-gray-700">{userData.nama_dokter}</Text>
 
             <Text className="font-bold text-lg text-skyDark mt-2">
               Username
             </Text>
 
-            <Text className="text-gray-700">{DataDummy.username}</Text>
+            <Text className="text-gray-700">{userData.username_dokter}</Text>
 
             <Text className="font-bold text-lg text-skyDark mt-2">Email</Text>
-            <Text className="text-gray-700">{DataDummy.email}</Text>
+            <Text className="text-gray-700">{userData.email_dokter}</Text>
 
             <Text className="font-bold text-lg text-skyDark mt-2">
               Nomor Telepon
             </Text>
-            <Text className="text-gray-700">{DataDummy.no_tlp}</Text>
+            <Text className="text-gray-700">{userData.notlp_dokter}</Text>
 
             <Text className="font-bold text-lg text-skyDark mt-2">
               Spesialis
             </Text>
-            <Text className="text-gray-700">{DataDummy.spesialis}</Text>
+            <Text className="text-gray-700">{userData.spesialis_dokter}</Text>
 
             {/* Ganti Password */}
             <Text className="font-bold text-lg text-skyDark mt-4">
